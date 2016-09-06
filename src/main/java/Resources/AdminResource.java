4ebfab7054a1678e6a7642bb04028829c8d0225b
Juan Santiago Acevedo <js.acevedo10@uniandes.edu.co>
2016-09-06 06:20:15 -0500

    Register

---
+++ src/main/java/Resources/AdminResource.java
@@ -0,0 +1,66 @@
+package Resources;
+
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import DAO.UserDAO;
+import DTO.Admin;
+import Security.Roles;
+
+@Path("admin")
+public class AdminResource {
+
+public static final String KEY = "ADMIN";
+	
+	//------------------------------------------------
+	//----------------------GET-----------------------
+	//------------------------------------------------
+	
+	@RolesAllowed(Roles.ADMIN)
+	@Path("/{userId}")
+	@PermitAll
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getAdmin(@PathParam("adminId") String userId) {
+		return UserDAO.getAdmin(userId);
+	}
+	
+	//------------------------------------------------
+	//----------------------POST----------------------
+	//------------------------------------------------
+	
+	@PermitAll
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response addAdmin(Admin admin) {
+		return UserDAO.addAdmin(admin);
+	}
+	
+	//------------------------------------------------
+	//----------------------PUT-----------------------
+	//------------------------------------------------
+	
+	@RolesAllowed(Roles.ADMIN)
+	@PUT
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response updateAdmin(Admin admin) {
+		return UserDAO.updateAdmin(admin);
+	}
+	
+	//------------------------------------------------
+	//----------------------DELETE--------------------
+	//------------------------------------------------
+	
+	
+}
---
+++ src/main/java/Resources/ManagerResource.java
@@ -0,0 +1,64 @@
+package Resources;
+
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import DAO.UserDAO;
+import DTO.Manager;
+import Security.Roles;
+
+@Path("manager")
+public class ManagerResource {
+
+	//------------------------------------------------
+	//----------------------GET-----------------------
+	//------------------------------------------------
+
+	@RolesAllowed({Roles.ADMIN, Roles.MANAGER})
+	@Path("/{userId}")
+	@PermitAll
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getManager(@PathParam("managerId") String userId) {
+		return UserDAO.getManager(userId);
+	}
+
+	//------------------------------------------------
+	//----------------------POST----------------------
+	//------------------------------------------------
+
+	@PermitAll
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response addManager(Manager manager) {
+		return UserDAO.addManager(manager);
+	}
+
+	//------------------------------------------------
+	//----------------------PUT-----------------------
+	//------------------------------------------------
+
+	@RolesAllowed({Roles.ADMIN, Roles.MANAGER})
+	@PUT
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response updateManager(Manager manager) {
+		return UserDAO.updateManager(manager);
+	}
+
+	//------------------------------------------------
+	//----------------------DELETE--------------------
+	//------------------------------------------------
+
+
+}
